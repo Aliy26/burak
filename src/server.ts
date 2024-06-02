b50f9c7aba22bf8b094ea571e71dc5f11b0067fa
Umar_Aliy <umaraliy092@gmail.com>
2024-06-03 07:14:12 +0900

    BRR Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("Activated!");
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD hh:mm");
+console.log(currentTime);
+
+const person: string = "Aliy";
+const count: number = 100;
