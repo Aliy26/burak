fdb73753312437bc7e11486fda31ea8088ce3181
Umar_Aliy <umaraliy092@gmail.com>
2024-06-08 19:59:03 +0900

    feat: member service model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import MemberService from "../model/Member.service";
 
 const restaurantController: T = {};
 
