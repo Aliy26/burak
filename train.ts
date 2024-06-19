e736055befb849f37d08787862a827fe2e712e22
Umar_Aliy <umaraliy092@gmail.com>
2024-06-19 10:30:47 +0900

    feat: developed admin frontend pages and integrated ejs header & footer files

--- train.ts
+++ train.ts
@@ -1,12 +1,20 @@
-/* Project Standars:
+// J-TASK:
 
- -Login Standards
- - Naming Standards {
-    funtionm, methodm, variables => Camel goHome
-    class => PASCAL       MemberService
-    folder =>             KEBAB
-    css => SNAKE          button_style
+// Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+// MASALAN: findLongestWord("I come from Uzbekistan") return "Uzbekistan"
+
+// @MITASK
+let empty: string = "";
+let longestWord: number = 0;
+function findLongestWord(str: string): string {
+  let toArr = str.split(" ");
+  for (let i = 0; i < toArr.length; i++) {
+    if (toArr[i].length > longestWord)
+      (longestWord = toArr[i].length), (empty = toArr[i]);
   }
- -Error handling
- 
-*/
+  return empty;
+}
+
+console.log(
+  findLongestWord("I am not from United-States I come from Uzbekistan")
+);
