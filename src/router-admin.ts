e5f258737de6c5f0b9de742f529fa927af52b1ac
Umar_Aliy <umaraliy092@gmail.com>
2024-07-02 01:47:05 +0900

    feat: developed create new product frontend logic

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -33,7 +33,7 @@ routerAdmin.get(
 routerAdmin.post(
   "/product/create",
   restaurantController.verifyRestaurant,
-  makeUploader("products").array("productImages", 4),
+  makeUploader("products").array("productImages", 5),
   productController.createNewProduct
 );
 routerAdmin.post(
