d4b99f8e3eb22e6d98e4e2fffceb27646effd329
Umar_Aliy <umaraliy092@gmail.com>
2024-07-04 16:13:25 +0900

    TASK-Q

--- train.ts
+++ train.ts
@@ -1,8 +1,10 @@
-// P-TASK:
+// Q-TASK:
 
-// Shunday function yozing, u object qabul qilsin va arrayni object arrayga otkazib arrayni qaytarsin qaytarsin.
-// MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
+// Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object, ikkinchisi string. Agar string parametr objectni propertysi bolsa true bolmasa false qaytarsin.
+// MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") return true; hasProperty({name: "BMW", model: "M3"}, "year") return false
 
-const objectToArray = (obj: {}): any[] => Object.entries(obj);
+const hasProperty = (obj: {}, str: string): boolean =>
+  Object.entries(obj).flat().includes(str) ? true : false;
 
-console.log(objectToArray({ a: 10, b: 20 }));
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model"));
+console.log(hasProperty({ name: "BMW", model: "M3" }, "year"));
