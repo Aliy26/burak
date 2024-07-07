a4668a9df08edd6fc000d4a5e387c4bcbaad7f23
Umar_Aliy <umaraliy092@gmail.com>
2024-07-08 01:10:32 +0900

    S-TASK

--- train.ts
+++ train.ts
@@ -1,11 +1,19 @@
-// R-TASK:
+// S-TASK:
 
-// Shunday function yozing, u string parametrga ega bolsin. String "1+2" holatda pass qilinganda string ichidagi sonlar yigindisini number holatda qaytarsin.
-// MASALAN: calculate("1+3") return 4;
+// Shunday function yozing, u numberlardan tashkil topgan array qabul qilsin va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
+// MASALAN: missingNumber([3, 0, 1]) return 2
 
-function calculate(str: string): number {
-  const arr = str.split(" ");
-  return +arr[0] + +arr[arr.length - 1];
+function missingNumber(arr: number[]): any {
+  let vacant: number[] = [];
+  const sorted = arr.sort();
+  const min = sorted[0];
+  const max = sorted[sorted.length - 1];
+  for (let i = min; i <= max; i++) {
+    if (!arr.includes(i)) vacant.push(i);
+  }
+  if (vacant.length > 1) return vacant;
+  else return Number(vacant.join(""));
 }
 
-console.log(calculate("1 + 3"));
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([4, 0, 1]));
