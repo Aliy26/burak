8587d43aa5ea9f2d820651b59617de5e91aba530
Umar_Aliy <umaraliy092@gmail.com>
2024-07-20 21:01:54 +0900

    X-TASK

--- train.ts
+++ train.ts
@@ -1,20 +1,36 @@
-// W-TASK:
+// X-TASK:
 
-// Shunday function yozing, uni array va number parametrlari bolsin. Function arrayni numberda berilgan uzunlikda kesib bolaklarga ajratilgan array holatida qaytarsin
-// MASALAN: chunkArray([1,2,3,4,5,6,7,8,9,10], 3) return [[1,2,3], [4,5,6], [7,8,9], [10]]
+//  Shunday function yozing, uni object va string parapetrlari bolsin. Function string parametri object ichida necha marotaba takrorlanganligini qaytarsin (nested object bolsa ham sanasin)
+//  MASALAN: countOccurrences({model: 'Bugatti', steer: {model: 'HANKOOK', size: 30}}, 'model') return 2
 
-// @MITASK
+function countOccurrences(obj: {}, str: string): number {
+  let vacant: any = [];
+  let vacant2: any = [];
+  let count = 0;
+  const arr1 = Object.keys(obj);
+  const arr2 = Object.values(obj);
 
-function chunkArray(arr: number[], num: number): number[] {
-  const vacant: number[] = [];
-
-  for (let i = 0; i < arr.length; i += num) {
-    const chunk: any = arr.slice(i, i + num);
-
-    vacant.push(chunk);
+  for (let value of arr2) {
+    if (typeof value === "object") vacant.push(value);
+    else if (typeof value === "string") arr1.push(value);
   }
+  vacant2 = Object.entries(vacant[0]);
 
-  return vacant;
+  const arr3 = arr1.concat(vacant2).flat();
+  for (let value of arr3) {
+    if (value === str) count++;
+  }
+  console.log(arr3);
+
+  return count;
 }
 
-console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(
+  countOccurrences(
+    {
+      model: "Bugatti",
+      steer: { model: "HANKOOK", size: 30 },
+    },
+    "model"
+  )
+);
