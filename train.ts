e90ebfa3a2aec0f215eaf593923e2b2856c1206c
Umar_Aliy <umaraliy092@gmail.com>
2024-07-27 22:53:31 +0900

    TASK-ZA & TASK-ZB

--- train.ts
+++ train.ts
@@ -1,15 +1,33 @@
-// Z-TASK:
+// ZA-TASK:
+// Shunday function yozing, u array ichidagi objectlarni “age” qiymati boyicha sortlab bersin.
+// MASALAN: sortByAge([{age:23}, {age:21}, {age:13}]) return [{age:13}, {age:21}, {age:23}]
 
-// Shunday function yozing, uni sonlardan tashkil topgan array qabul qilsin. Function arraydagi juft sonlarni yigindisini qaytarsin
-// MASALAN: sumEvens([1,2,3]) return 2
+function sortByAge(arr: any[]): number[] {
+  let vacant: any = [];
+  for (let num of arr) vacant.push(num.age);
+  vacant.sort((a: number, b: number) => {
+    return a - b;
+  });
 
-function sumEvens(arr: number[]): number | number[] {
-  let vacant: number[] = [];
-  for (let number of arr) {
-    if (number % 2 === 0) vacant.push(number);
-  }
-  return vacant.length > 1 ? vacant : Number(vacant);
+  arr.map((value, index) => {
+    return (value.age = vacant[index]);
+  });
+
+  return arr;
 }
 
-console.log(sumEvens([1, 2, 3]));
-console.log(sumEvens([1, 2, 3, 4]));
+console.log(
+  "TASK-ZA =>",
+  sortByAge([{ age: 111 }, { age: 23 }, { age: 21 }, { age: 13 }])
+);
+
+// ZB-TASK:
+
+// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
+// MASALAN: randomBetween(30, 50) return 45
+
+function randomBetween(from: number, until: number): number {
+  return Math.floor(Math.random() * (until - from)) + from;
+}
+console.log("\n");
+console.log("TASK-ZB =>", randomBetween(30, 50));
