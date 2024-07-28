1f92e5f78897aef8e40c459f880ba69de131752a
Umar_Aliy <umaraliy092@gmail.com>
2024-07-28 23:40:19 +0900

    TASK-ZC

--- train.ts
+++ train.ts
@@ -1,33 +1,9 @@
-// ZA-TASK:
-// Shunday function yozing, u array ichidagi objectlarni “age” qiymati boyicha sortlab bersin.
-// MASALAN: sortByAge([{age:23}, {age:21}, {age:13}]) return [{age:13}, {age:21}, {age:23}]
+// ZC-TASK:
 
-function sortByAge(arr: any[]): number[] {
-  let vacant: any = [];
-  for (let num of arr) vacant.push(num.age);
-  vacant.sort((a: number, b: number) => {
-    return a - b;
-  });
+// Shunday function yozing, uni number parametri bolsin va function qabul parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin
+// MASALAN: celsiusToFahrenheit(0) return 32
 
-  arr.map((value, index) => {
-    return (value.age = vacant[index]);
-  });
+const celsiusToFahrenheit = (num: number): number => (num * 9) / 5 + 32;
 
-  return arr;
-}
-
-console.log(
-  "TASK-ZA =>",
-  sortByAge([{ age: 111 }, { age: 23 }, { age: 21 }, { age: 13 }])
-);
-
-// ZB-TASK:
-
-// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
-// MASALAN: randomBetween(30, 50) return 45
-
-function randomBetween(from: number, until: number): number {
-  return Math.floor(Math.random() * (until - from)) + from;
-}
-console.log("\n");
-console.log("TASK-ZB =>", randomBetween(30, 50));
+console.log(0, "deg Celisius in Fahrenheit =>", celsiusToFahrenheit(0));
+console.log(36, "deg Celisius in Fahrenheit =>", celsiusToFahrenheit(36));
