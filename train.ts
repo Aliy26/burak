e0dccd56d9c1070486de21b18d51e1a51eba4e03
Umar_Aliy <umaraliy092@gmail.com>
2024-08-10 16:21:56 +0900

    TASK-ZH

--- train.ts
+++ train.ts
@@ -1,9 +1,21 @@
-// ZG-TASK:
+// ZH-TASK:
 
-// Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin.
-// MASALAN: capitalizeWords('name should be a string') return 'name_should_be_a_string'
+// Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin.
+// MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
 
-const capitalizeWords = (str: string): string =>
-  (str = str.split(" ").join("_"));
+function findDisappearedNumbers(arr: number[]): any {
+  arr = arr.sort((a, b) => {
+    return a - b;
+  });
+  let min: number = arr[0];
+  let max: number = arr[arr.length - 1];
+  let vacant: number[] = [];
 
-console.log(capitalizeWords("name should be a string"));
+  for (let i = min; i < max; i++) {
+    if (!arr.includes(i)) vacant.push(i);
+  }
+  return vacant;
+}
+
+console.log(findDisappearedNumbers([1, 3, 5, 7]));
+console.log(findDisappearedNumbers([11, 9, 8, 7, 1]));
