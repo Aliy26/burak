2ea0607cd4032e581692942d1ee380c4cec663e0
Umar_Aliy <umaraliy092@gmail.com>
2024-08-13 12:53:35 +0900

    TASK-ZI

--- train.ts
+++ train.ts
@@ -1,21 +1,12 @@
-// ZH-TASK:
+// ZI-TASK:
 
-// Shunday function yozing, u berilgan array parametrni ichidagi eng katta raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin.
-// MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
+// Shunday function yozing, u function ishga tushgandan 3 soniyadan keyin "Hello World" ni qaytarsin.
+// MASALAN: delayHelloWorld("Hello World") return "Hello World"
 
-function findDisappearedNumbers(arr: number[]): any {
-  arr = arr.sort((a, b) => {
-    return a - b;
-  });
-  let min: number = arr[0];
-  let max: number = arr[arr.length - 1];
-  let vacant: number[] = [];
-
-  for (let i = min; i < max; i++) {
-    if (!arr.includes(i)) vacant.push(i);
-  }
-  return vacant;
+function delayHelloWorld(str: string): void {
+  setTimeout(() => {
+    console.log(str);
+  }, 3000);
 }
 
-console.log(findDisappearedNumbers([1, 3, 5, 7]));
-console.log(findDisappearedNumbers([11, 9, 8, 7, 1]));
+delayHelloWorld("Hello World");
